ceb5f2940906fb164596f888e1e7ac3cf12f2e43
pabluchenko <jp.junco@uniandes.edu.co>
2023-09-24 02:50:58 -0500

    dir's creation

--- src/main/java/uniandes/edu/co/hotelAndes/ProyectoApplication.java
+++ src/main/java/uniandes/edu/co/hotelAndes/ProyectoApplication.java
@@ -1,4 +1,4 @@
-package uniandes.edu.co.proyecto;
+package uniandes.edu.co.hotelAndes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
