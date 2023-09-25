de1d3a58b191806ccb1ea6af9d28852dc0f2f4d1
pabluchenko <jp.junco@uniandes.edu.co>
2023-09-25 15:10:30 -0500

    few other  things

--- data/DDL/ddl.sql
+++ data/DDL/ddl.sql
@@ -1,24 +1,25 @@
 CREATE TABLE cuentas (
-                         habitacion_numero NUMBER NOT NULL,
-                         hotel_nit         NUMBER NOT NULL,
-                         servicio_id       NUMBER NOT NULL
+                         habitaciones_numero NUMBER NOT NULL,
+                         habitaciones_nit    NUMBER NOT NULL,
+                         servicios_id        NUMBER NOT NULL
 );
 
-ALTER TABLE cuentas ADD CONSTRAINT cuentas_pk PRIMARY KEY ( habitacion_numero,
-                                                            hotel_nit );
+ALTER TABLE cuentas ADD CONSTRAINT cuentas_pk PRIMARY KEY ( habitaciones_numero,
+                                                            habitaciones_nit );
 
 CREATE TABLE habitaciones (
-                              numero     NUMBER NOT NULL,
-                              disponible VARCHAR2(15),
-                              hotel_nit  NUMBER NOT NULL,
-                              reserva_id NUMBER NOT NULL
+                              numero                NUMBER NOT NULL,
+                              disponible            VARCHAR2(15),
+                              hoteles_nit           NUMBER NOT NULL,
+                              tipos_habitaciones_id NUMBER NOT NULL,
+                              reservas_id           NUMBER NOT NULL
 );
 
 ALTER TABLE habitaciones
     ADD CHECK ( disponible IN ( 'NO', 'SI' ) );
 
 ALTER TABLE habitaciones ADD CONSTRAINT habitaciones_pk PRIMARY KEY ( numero,
-                                                                      hotel_nit );
+                                                                      hoteles_nit );
 
 CREATE TABLE hoteles (
                          nombre VARCHAR2(50),
@@ -28,25 +29,24 @@ CREATE TABLE hoteles (
 ALTER TABLE hoteles ADD CONSTRAINT hoteles_pk PRIMARY KEY ( nit );
 
 CREATE TABLE ocupadas (
-                          habitacion_numero        NUMBER NOT NULL,
-                          hotel_nit                NUMBER NOT NULL,
-                          usuario_numero_documento VARCHAR2(25) NOT NULL
+                          habitaciones_numero       NUMBER NOT NULL,
+                          habitaciones_nit          NUMBER NOT NULL,
+                          usuarios_numero_documento VARCHAR2(25) NOT NULL
 );
 
 ALTER TABLE ocupadas
-    ADD CONSTRAINT ocupadas_pk PRIMARY KEY ( habitacion_numero,
-                                             hotel_nit,
-                                             usuario_numero_documento );
+    ADD CONSTRAINT ocupadas_pk PRIMARY KEY ( habitaciones_numero,
+                                             habitaciones_nit,
+                                             usuarios_numero_documento );
 
 CREATE TABLE planes_consumo (
-                                nombre          VARCHAR2(50),
-                                plan_consumo_id NUMBER NOT NULL,
-                                hotel_nit       NUMBER NOT NULL
+                                id          NUMBER NOT NULL,
+                                nombre      VARCHAR2(50),
+                                hoteles_nit NUMBER NOT NULL
 );
 
-ALTER TABLE planes_consumo ADD CONSTRAINT planes_consumo_pk PRIMARY KEY ( plan_consumo_id );
-
-ALTER TABLE planes_consumo ADD CONSTRAINT planes_consumo_nit_un UNIQUE ( hotel_nit );
+ALTER TABLE planes_consumo ADD CONSTRAINT planes_consumo_pk PRIMARY KEY ( hoteles_nit,
+                                                                          id );
 
 CREATE TABLE reservas (
                           id            NUMBER NOT NULL,
@@ -61,22 +61,22 @@ CREATE TABLE servicios (
                            id                       NUMBER NOT NULL,
                            horario_servicio_inicial VARCHAR2(50),
                            horario_servicio_final   VARCHAR2(50),
-                           tipo_servicio_id         NUMBER NOT NULL,
-                           reserva_id               NUMBER NOT NULL
+                           tipos_servicios_id       NUMBER NOT NULL,
+                           reservas_id              NUMBER NOT NULL
 );
 
 ALTER TABLE servicios ADD CONSTRAINT servicios_pk PRIMARY KEY ( id );
 
 CREATE TABLE tipos_habitaciones (
+                                    id        NUMBER NOT NULL,
                                     tipo      VARCHAR2(55),
-                                    capacidad VARCHAR2(15),
-                                    hotel_nit NUMBER NOT NULL
+                                    capacidad VARCHAR2(15)
 );
 
 ALTER TABLE tipos_habitaciones
     ADD CHECK ( tipo IN ( 'LARGA_ESTADIA', 'PROMOCION', 'TIEMPO_COMPARTIDO', 'TODO_INCLUIDO' ) );
 
-ALTER TABLE tipos_habitaciones ADD CONSTRAINT tipos_habitaciones_pk PRIMARY KEY ( hotel_nit );
+ALTER TABLE tipos_habitaciones ADD CONSTRAINT tipos_habitaciones_pk PRIMARY KEY ( id );
 
 CREATE TABLE tipos_servicios (
                                  id     NUMBER NOT NULL,
@@ -102,45 +102,46 @@ ALTER TABLE usuarios
 ALTER TABLE usuarios ADD CONSTRAINT usuarios_pk PRIMARY KEY ( numero_documento );
 
 ALTER TABLE cuentas
-    ADD CONSTRAINT cuentas_habitaciones_fk FOREIGN KEY ( habitacion_numero,
-                                                         hotel_nit )
+    ADD CONSTRAINT cuentas_habitaciones_fk FOREIGN KEY ( habitaciones_numero,
+                                                         habitaciones_nit )
         REFERENCES habitaciones ( numero,
-                                  hotel_nit );
+                                  hoteles_nit );
 
 ALTER TABLE cuentas
-    ADD CONSTRAINT cuentas_servicios_fk FOREIGN KEY ( servicio_id )
+    ADD CONSTRAINT cuentas_servicios_fk FOREIGN KEY ( servicios_id )
         REFERENCES servicios ( id );
 
 ALTER TABLE habitaciones
-    ADD CONSTRAINT habitaciones_hoteles_fk FOREIGN KEY ( hotel_nit )
+    ADD CONSTRAINT habitaciones_hoteles_fk FOREIGN KEY ( hoteles_nit )
         REFERENCES hoteles ( nit );
 
 ALTER TABLE habitaciones
-    ADD CONSTRAINT habitaciones_reservas_fk FOREIGN KEY ( reserva_id )
+    ADD CONSTRAINT habitaciones_reservas_fk FOREIGN KEY ( reservas_id )
         REFERENCES reservas ( id );
 
-ALTER TABLE ocupadas
-    ADD CONSTRAINT ocupadas_habitaciones_fk FOREIGN KEY ( habitacion_numero,
-                                                          hotel_nit )
-        REFERENCES habitaciones ( numero,
-                                  hotel_nit );
+--  ERROR: FK name length exceeds maximum allowed length(30)
+ALTER TABLE habitaciones
+    ADD CONSTRAINT habi_tipos_habi_fk FOREIGN KEY ( tipos_habitaciones_id )
+        REFERENCES tipos_habitaciones ( id );
 
 ALTER TABLE ocupadas
-    ADD CONSTRAINT ocupadas_usuarios_fk FOREIGN KEY ( usuario_numero_documento )
+    ADD CONSTRAINT ocupadas_habitaciones_fk FOREIGN KEY ( habitaciones_numero,
+                                                          habitaciones_nit )
+        REFERENCES habitaciones ( numero,
+                                  hoteles_nit );
+
+ALTER TABLE ocupadas
+    ADD CONSTRAINT ocupadas_usuarios_fk FOREIGN KEY ( usuarios_numero_documento )
         REFERENCES usuarios ( numero_documento );
 
 ALTER TABLE planes_consumo
-    ADD CONSTRAINT planes_consumo_hoteles_fk FOREIGN KEY ( hotel_nit )
+    ADD CONSTRAINT planes_consumo_hoteles_fk FOREIGN KEY ( hoteles_nit )
         REFERENCES hoteles ( nit );
 
 ALTER TABLE servicios
-    ADD CONSTRAINT servicios_reservas_fk FOREIGN KEY ( reserva_id )
+    ADD CONSTRAINT servicios_reservas_fk FOREIGN KEY ( reservas_id )
         REFERENCES reservas ( id );
 
 ALTER TABLE servicios
-    ADD CONSTRAINT servicios_tipos_servicios_fk FOREIGN KEY ( tipo_servicio_id )
+    ADD CONSTRAINT servicios_tipos_servicios_fk FOREIGN KEY ( tipos_servicios_id )
         REFERENCES tipos_servicios ( id );
-
-ALTER TABLE tipos_habitaciones
-    ADD CONSTRAINT tipos_habitaciones_hoteles_fk FOREIGN KEY ( hotel_nit )
-        REFERENCES hoteles ( nit );
