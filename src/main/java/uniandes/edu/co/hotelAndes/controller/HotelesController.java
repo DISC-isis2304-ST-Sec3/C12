d52e521fb828bcf6cd7c7882305c453d8c6562a8
pabluchenko <jp.junco@uniandes.edu.co>
2023-09-28 10:52:01 -0500

    hoteles CRUD final

--- src/main/java/uniandes/edu/co/hotelAndes/controller/HotelesController.java
+++ src/main/java/uniandes/edu/co/hotelAndes/controller/HotelesController.java
@@ -4,6 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import uniandes.edu.co.hotelAndes.modelo.Hotel;
+import uniandes.edu.co.hotelAndes.modelo.Usuario;
 import uniandes.edu.co.hotelAndes.repositorio.HotelRepository;
 
 @Controller
@@ -19,5 +24,45 @@ public class HotelesController {
         return "hoteles";
     }
 
+    @GetMapping("/hoteles/new")
+    public String hotelForm(Model model) {
+        model.addAttribute("hotel", new Hotel());
+        return "hotelNuevo";
+    }
+
+    @PostMapping("/hoteles/new/save")
+    public String hotelGuardar(@ModelAttribute Hotel hotel) {
+
+        hotelRepository.insertarHotel(hotel.getNombre(), hotel.getNit());
+
+        return "redirect:/hoteles";
+    }
+
+    @GetMapping("/hoteles/{id}/edit")
+    public String hotelEditarForm(@PathVariable("id") long id, Model model) {
+
+        Hotel hotel = hotelRepository.darHotel(id);
+
+        if (hotel != null) {
+            model.addAttribute("hotel", hotel);
+            return "hotelEditar";
+        } else {
+            return "redirect:/hoteles";
+        }
+    }
+
+    @PostMapping("/hoteles/{id}/edit/save")
+    public String hotelEditarGuardar(@PathVariable("id") long id, @ModelAttribute Hotel hotel) {
+
+        hotelRepository.actualizarHotel( id, hotel.getNombre(), hotel.getNit());
+        return "redirect:/hoteles";
+    }
+
+    @GetMapping("/hoteles/{id}/delete")
+    public String hotelBorrar(@PathVariable("id") long id) {
+
+        hotelRepository.eliminarHotel(id);
+        return "redirect:/hoteles";
+    }
 
 }
